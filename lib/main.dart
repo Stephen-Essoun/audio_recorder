496bf8673098b60fcf637ac544f0fb586a4a0a5d
Stephen Essoun <boit7845@gmail.com>
2023-06-16 00:02:53 +0000

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'audio.player.dart';
+import 'audio.recorder.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool showPlayer = false;
+  String? audioPath;
+
+  @override
+  void initState() {
+    showPlayer = false;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.red),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Record and Play')),
+        body: Center(
+          child: showPlayer
+              ? Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: AudioPlayer(
+                    source: audioPath!,
+                    onDelete: () {
+                      setState(() => showPlayer = false);
+                    },
+                  ),
+                )
+              : AudioRecorder(
+                  onStop: (path) {
+                    if (kDebugMode) print('Recorded file path: $path');
+                    setState(() {
+                      audioPath = path;
+                      showPlayer = true;
+                    });
+                  },
+                ),
+        ),
+      ),
+    );
+  }
+}
